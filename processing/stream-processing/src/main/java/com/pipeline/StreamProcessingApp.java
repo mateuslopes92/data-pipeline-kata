80fc5d80850b1f31c3fb36aa6573a3630a3ebfef
mateuslopes92 <mateuslopes92@gmail.com>
2026-03-18 00:59:51 -0300

    Refactor on streamprocessingapp

--- processing/stream-processing/src/main/java/com/pipeline/StreamProcessingApp.java
+++ processing/stream-processing/src/main/java/com/pipeline/StreamProcessingApp.java
@@ -1,5 +1,7 @@
 package com.pipeline;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.*;
 import org.apache.kafka.streams.kstream.*;
@@ -18,24 +20,41 @@ public class StreamProcessingApp {
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
         StreamsBuilder builder = new StreamsBuilder();
+        ObjectMapper mapper = new ObjectMapper();
 
-        KStream<String, String> dbStream =
-                builder.stream("sales-db");
-
-        KStream<String, String> fileStream =
-                builder.stream("sales-file");
-
-        KStream<String, String> apiStream =
-                builder.stream("sales-api");
+        KStream<String, String> dbStream = builder.stream("sales-db");
+        KStream<String, String> fileStream = builder.stream("sales-file");
+        KStream<String, String> apiStream = builder.stream("sales-api");
 
         KStream<String, String> mergedStream =
                 dbStream.merge(fileStream).merge(apiStream);
 
-        mergedStream.peek((key, value) ->
-                System.out.println("Processing: " + value)
-        );
+        KTable<String, Integer> salesByCity =
+                mergedStream
+                        .map((key, value) -> {
+                            try {
 
-        mergedStream.to("sales-processed");
+                                JsonNode node = mapper.readTree(value);
+
+                                String city = node.get("city").asText();
+                                int amount = node.get("amount").asInt();
+
+                                return KeyValue.pair(city, amount);
+
+                            } catch (Exception e) {
+                                return KeyValue.pair("unknown", 0);
+                            }
+                        })
+                        .groupByKey(Grouped.with(Serdes.String(), Serdes.Integer()))
+                        .reduce(Integer::sum);
+
+                        salesByCity
+                        .toStream()
+                        .peek((city, total) ->
+                                System.out.println("TOTAL SALES -> " + city + " = " + total)
+                        )
+                        .mapValues(total -> String.valueOf(total))
+                        .to("sales-by-city", Produced.with(Serdes.String(), Serdes.String()));
 
         KafkaStreams streams = new KafkaStreams(builder.build(), props);
 
